cc8bada227a7df99d6535756172c8b1b05376cfa
DieroKreator <qtechwired@gmail.com>
2025-04-07 22:40:24 -0300

    refactor test for backpack purchase flow to improve URL and title assertions

--- tests/comprarMochila.spec.js
+++ tests/comprarMochila.spec.js
@@ -1,12 +1,24 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('Fluxo de compra da mochila', async ({ page }) => {
 
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('[name="password"]', 'secret_sauce');
-    await page.click('input.submit-button.btn_action');
+    await page.goto('https://www.saucedemo.com/')
+    await page.fill('#user-name', 'standard_user')
+    await page.fill('[name="password"]', 'secret_sauce')
+    await page.click('input.submit-button.btn_action')
 
-    await page.waitForTimeout(2000);
+    // await page.waitForTimeout(2000);
+    await expect(page).toHaveURL(/.*inventory/)
+    // await expect(page.locator('span.title').textContent(), 'Products');
+    const tituloSecao = page.locator('span.title')
+    expect(tituloSecao).toHaveText('Products')
+
+    await page.click('#item_4_title_link')
+
+    await expect(page).toHaveURL(/.*inventory-item/)
+    const tituloProduto = page.locator('#back-to-products')
+    expect(tituloProduto).toHaveText('Sauce Labs Backpack')
+    const precoProduto = page.locator('.inventory_details_price')
+    expect(precoProduto).toHaveText('$29.99')
 }
 );
