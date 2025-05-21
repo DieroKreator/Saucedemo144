d1cae7ee579bf101b7ba08550c823e63678adbdb
DieroKreator <qtechwired@gmail.com>
2025-05-21 16:33:26 -0600

    Add LoginPage class for user authentication functionality

---
+++ pages/Login.js
@@ -0,0 +1,22 @@
+const { expect } = require('@playwright');
+
+class LoginPage {
+    constructor(page) {
+        this.page = page;
+        this.txtUserName = page.locator('#user-name');
+        this.txtPassword = page.locator('[name="password"]');
+        this.btnLogin = page.locator('input.submit-button.btn_action');
+    }
+
+    async goto(url) {
+        await this.page.goto(url);
+    }
+
+    async login(username, password) {
+        await this.txtUserName.fill(username);
+        await this.txtPassword.fill(password);
+        await this.btnLogin.click();
+    }
+}
+
+module.exports = { LoginPage }; # it is important to export the class so it can be used in other files
