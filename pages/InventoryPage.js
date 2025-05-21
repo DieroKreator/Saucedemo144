42d82d8b60652c9f062a56938f555ab9c89a7be0
DieroKreator <qtechwired@gmail.com>
2025-05-21 16:38:27 -0600

    Add InventoryPage class for inventory management functionality

---
+++ pages/InventoryPage.js
@@ -0,0 +1,19 @@
+const { expect } = require('@playwright');
+
+class InventoryPage {
+    constructor(page) {
+        this.page = page;
+        this.tituloSecao = page.locator('span.title');
+        this.backpack = page.locator('#item_4_title_link');
+    }
+
+    async verificarInventoryPage () {
+        await expect(this.page).toHaveURL(/.*inventory/);
+        await expect(this.page).toHaveText('Products');
+    }
+
+    async clicarNaMochila () {
+        await this.backpack.click();
+    }
+}
+module.exports = { InventoryPage };
