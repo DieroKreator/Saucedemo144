cbaeac4bd85416ec0bd6a53871f831d9ad815fab
DieroKreator <qtechwired@gmail.com>
2025-05-22 17:07:35 -0600

    Comment out screenshot and video options in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,8 @@ module.exports = {
     use: {
         browserName: 'firefox',
         headless: false,
-        screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
+        // screenshot: 'only-on-failure',
+        // video: 'retain-on-failure',
     },
     reporter: [['html', { outputFolder: 'report', open: 'never' }]],
     timeout: 60000,
