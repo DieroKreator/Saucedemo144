3ca6ac4f379ead1b53d5497d3eedea5576278941
DieroKreator <qtechwired@gmail.com>
2025-05-21 21:25:07 -0600

    Comment out backpack locator and update clicarProduto method to use dynamic SKU for product selection

--- pages/InventoryPage.js
+++ pages/InventoryPage.js
@@ -4,7 +4,7 @@ class InventoryPage {
     constructor(page) {
         this.page = page;
         this.tituloSecao = page.locator('span.title');
-        this.backpack = page.locator('#item_4_title_link');
+        // this.backpack = page.locator('#item_4_title_link');
     }
 
     async verificarInventoryPage () {
@@ -13,7 +13,8 @@ class InventoryPage {
     }
 
     async clicarProduto () {
-        await this.backpack.click();
+        // await this.backpack.click();
+        this.page.locator(`#item_${sku}_title_link`).click();
     }
 }
 module.exports = { InventoryPage };
