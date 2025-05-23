61311f847927b40460ca892bb7e3214059e762a0
DieroKreator <qtechwired@gmail.com>
2025-05-22 18:44:01 -0600

    Add shopping cart functionality to InventoryItemPage class

--- pages/InventoryItemPage.js
+++ pages/InventoryItemPage.js
@@ -6,6 +6,7 @@ class InventoryItemPage {
         this.backButton = page.locator('#back-to-products');
         this.tituloProduto = page.locator('div.inventory_details_name.large_size');
         this.precoProduto = page.locator('.inventory_details_price');
+        this.shoppingCartBadge = this.page.locator('.shopping_cart_badge');
     }
 
     async verificarInventoryItemPage() {
@@ -16,6 +17,30 @@ class InventoryItemPage {
     async verificarTituloPrecoDoProduto(titulo, preco) {
         await expect(this.tituloProduto).toHaveText(titulo);
         await expect(this.precoProduto).toHaveText(preco);
+    }
+
+    async clicarNoBotaoAddToCart() {
+        await this.page.locator('#add-to-cart').click();
+    }
+
+    async verificarShoppingCartBadge(cartItemsCount) {
+        if (cartItemsCount > 0) {
+            await expect(this.shoppingCartBadge).toBeVisible();
+        } else {
+            await expect(this.shoppingCartBadge).toBeHidden();
+        }
+    }
+
+    async clicarNoBotaoRemove() {
+        const botaoRemove = this.page.locator('#remove');
+        await expect(botaoRemove).toHaveText('Remove');
+        await botaoRemove.click();
+    }
+
+    async clicarNoBotaoShoppingCart() {
+        const botaoShoppingCart = this.page.locator('.shopping_cart_link');
+        await expect(botaoShoppingCart).toHaveText('1');
+        await botaoShoppingCart.click();
     }
 }
 
